50300ed5ff7072e81a08e0a51cb91f212bb8430c
eniitanadeleke <eniitanadeleke@gmail.com>
2024-10-13 15:47:05 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+    // This script displays a welcome message when the page loads
+    window.onload = function() {
+        alert("Welcome to Ennyet Beauty Salon! We hope you find the perfect style.");
+    };

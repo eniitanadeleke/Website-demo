f5d0e29bcd0bb6f9a96551d4f4d20384a462b6af
eniitanadeleke <eniitanadeleke@gmail.com>
2024-10-13 15:44:41 -0700

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-    // This script displays a welcome message when the page loads
-    window.onload = function() {
-        alert("Welcome to Ennyet Beauty Salon! We hope you find the perfect style.");
-    };
